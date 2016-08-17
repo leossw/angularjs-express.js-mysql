70be8f8588c383cc68110d257312edd8e61d09f8
banana <178776885@qq.com>
2016-08-17 09:35:59 +0800

    client/accounts/controller/patient_info_add.js

--- client/accounts/controller/patient_info_add.js
+++ client/accounts/controller/patient_info_add.js
@@ -7,11 +7,11 @@ angular.module('mobilecare').controller('PatientInfoAddCtrl',
     vm.errorMessage = '';
 
     vm.patient = {
-    	serialNumber: '00201600000000',   //患者序列号？？(先设置一个默认值)
+    	serialNumber: setTimeToSerialNumber(),   //患者序列号？？(序列号不能重复,先用时间代替)
     	name: '',
     	sex: '',
     	birthday: vm.maxbirthday,
-    	countryCode: 156,
+    	countryCode: '156',
     	certificateType: '',
     	idCard: '',
     	fixedTel: '',
@@ -24,26 +24,121 @@ angular.module('mobilecare').controller('PatientInfoAddCtrl',
     	weight: 0,
     	bloodType: '',
     	mainDisease: '',
-    	registerTime: vm.maxbirthday,
-    	patientType: 01,    //默认患者类型01,个人患者(家庭患者)
-        organization_id: 1,   // 默认当为:'个人（家庭）患者'时，organization_id为特定值"1"
-        commonGuardianVerificationCode: '', // 普通监护人验证码
-        authorizationGuardianVerificationCode: '', // 授权监护人验证码
-        userId: '',
+    	registrar: $cookies.get('currentUserId'),
+    	registerTime: formatDateTime(),
+    	patientType: '01',    //默认患者类型01,个人患者(家庭患者)
+        organizationId: '1',   // 默认当为:'个人（家庭）患者'时，organization_id为特定值"1"
+        commonGuardianVerificationCode: getRandom(), // 普通监护人验证码
+        authorizationGuardianVerificationCode: getRandom(), // 授权监护人验证码
+        userId: '00',			//设定默认值00
         remark: ''
     }
- //   console.log($cookies.get('currentUserName'));
- //   console.log($cookies.get('currentUserId'));
-    //获取国籍代码
+
+
+//	console.log($cookies.get('currentUserName'));
+//	console.log($cookies.get('currentUserId'));
+ 
+//获取国籍代码
     $http.get('/api/CountryCode').success(function (data){
     	vm.countryCodes = data;  
     }).error(function (data){
     	console.log('无法获取国籍列表');
     });
 
+//获取10位随机数为监护人验证码
+    function getRandom(){
+		var randomNum = Math.random();
+		if(randomNum >= 0.1){
+			return Math.floor(randomNum*10000000000);
+		}
+		else{
+			return Math.floor((randomNum+0.1)*10000000000);
+		}
+	}
 
+//时间转换成字符串，设置为serialNumber
+	function setTimeToSerialNumber(){
+		var time = new Date(),
+			year = time.getFullYear(),
+			month = time.getMonth()+1,
+			day = time.getDate(),
+			hour = time.getHours(),
+			minute = time.getMinutes(),
+			seconds = time.getSeconds();
+		return year.toString()+(month>9?month:'0'+month)+(day>9?day:'0'+day)+(hour>9?hour:'0'+hour)+(minute>9?minute:'0'+minute)+(seconds>9?seconds:'0'+seconds);
+	}
+
+//转换日期格式
+	function formatDateTime() {  
+		var date = new Date();
+   		var y = date.getFullYear();
+   		var m = date.getMonth()+1;
+    	m = m<10?('0'+m):m;
+    	var d = date.getDate();
+    	d = d<10?('0'+d):d;
+   		var h = date.getHours();
+    	var minute = date.getMinutes();
+    	minute = minute<10?('0'+minute):minute;
+    	var seconds = date.getSeconds();
+    	seconds = seconds<10?('0'+seconds):seconds;
+    	return y + '-' + m + '-' + d+' '+h+':'+minute+':'+seconds;  
+};  
+
+//添加用户信息
     vm.addInfo = function (){
+    	var patientInfo = {
+    	serialNumber: vm.patient.serialNumber,
+    	name: vm.patient.name,
+    	sex: vm.patient.sex,
+    	birthday: vm.patient.birthday,
+    	countryCode: vm.patient.countryCode,
+    	certificateType: vm.patient.certificateType,
+    	idCard: vm.patient.idCard,
+    	fixedTel: vm.patient.fixedTel,
+    	mobilePhone: vm.patient.mobilePhone,
+    	email: vm.patient.email,
+    	weChat: vm.patient.weChat,
+    	address: vm.patient.address,
+    	postalCode: vm.patient.postalCode,
+    	height: vm.patient.height,
+    	weight: vm.patient.weight,
+    	bloodType: vm.patient.bloodType,
+    	mainDisease: vm.patient.mainDisease,
+    	registrar: vm.patient.registrar,
+    	registerTime: vm.patient.registerTime,
+    	patientType: vm.patient.patientType,
+        organizationId: vm.patient.organizationId,
+        commonGuardianVerificationCode: vm.patient.commonGuardianVerificationCode, // 普通监护人验证码
+        authorizationGuardianVerificationCode: vm.patient.authorizationGuardianVerificationCode, // 授权监护人验证码
+        userId: vm.patient.userId,
+        remark: vm.patient.remark
+    	}
 
+    	$http.post('/api/register/addPatientInfo',patientInfo)
+    		.success(function (data){
+    			if( data == '信息添加成功'){
+    				vm.errorMessage = data;
+    				vm.showAlert();
+    				$state.go('registernb');
+    			}
+    			else{
+    				vm.errorMessage = '信息添加失败';
+    				vm.showAlert();
+    				$state.go('login');
+    			}
+    		})
+    		.error(function (data){
+    			console.log('用户信息添加失败');
+    		});
+   		}
+
+//弹出提示框
+    vm.showAlert = function (){
+    	$mdDialog.show(
+    		$mdDialog.alert()
+    			.clickOutsideToClose(true)
+    			.textContent(vm.errorMessage)
+    			.ok('确定')
+    	);
     }
-
 }]);
