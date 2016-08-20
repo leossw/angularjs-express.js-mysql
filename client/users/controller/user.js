49e498dc7edad7d7e28aa6472f55ff9aa319bc2f
banana <178776885@qq.com>
2016-08-20 13:21:17 +0800

    client

---
+++ client/users/controller/user.js
@@ -0,0 +1,24 @@
+angular.module('mobilecare').controller('UserCtrl',
+	['$scope','$state','$cookies','$state',function ($scope,$state,$cookies,$state){
+    
+    var vm = this;
+
+    vm.userType = $cookies.get('currentUserType');
+    vm.userName = $cookies.get('currentUserName');
+    vm.userNickname = $cookies.get('currentUserNickname');
+
+//获取当前用户类型
+	vm.isPatient = (vm.userType == '01')? true : false;    
+//	vm.isGuardian = (vm.userType == '02')? true : false;
+
+//跳转到个人信息界面
+    vm.goToPersonalInfo = function (){
+    	if(vm.isPatient){
+    		$state.go('personalinfopatient');
+    	}
+    	else{
+    		$state.go('personalinfoguardian');
+    	}
+    }
+
+}]);
