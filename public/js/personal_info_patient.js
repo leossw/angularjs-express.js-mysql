24d36b9965c4ee8317d984b11173105874f07f11
banana <178776885@qq.com>
2016-08-21 11:19:13 +0800

    public

--- public/js/personal_info_patient.js
+++ public/js/personal_info_patient.js
@@ -2,9 +2,13 @@ var pool = require('./pool');
 
 exports.getPatientInfo = function (req,res){
 	pool.getConnection(function (err,connection){
-		connection.query('SELECT * FROM patient WHERE user_name = ?',[req.body.userName],function (err,results){
-
-
+		connection.query('SELECT * FROM patient WHERE registrar = ?',[req.body.registrar],function (err,results){
+			if(err){
+				console.log(err.message);
+			}
+			else{
+				res.send(results[0]);
+			}
 			connection.release();
 		});
 	});
