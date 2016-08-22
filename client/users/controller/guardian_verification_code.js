dceca3a456fb612e499a48484b57524df1294b1c
banana <178776885@qq.com>
2016-08-23 07:51:23 +0800

    client

--- client/users/controller/guardian_verification_code.js
+++ client/users/controller/guardian_verification_code.js
@@ -1,6 +1,18 @@
 angular.module('mobilecare').controller('GuardianVerificationCodeCtrl',
-	['$scope','$state',function ($scope,$state){
+	['$scope','$http','$cookies',function ($scope,$http,$cookies){
     
     var vm = this;
 
+    vm.registrar = $cookies.get('currentUserId');	//获取用户ID
+
+//发送请求获取监护人授权码
+    $http.post('/api/getGuardianVerificationCode',{registrar: vm.registrar})
+    	.success(function (data){
+//    		console.log(data);
+    		vm.authorizationGuardianCode = data.authorization_guardian_verification_code;
+    		vm.commonGuardianCode = data.common_guardian_verification_code;
+    	})
+    	.error(function (data){
+    		console.log('无法获取监护人授权码');
+    	});
 }]);
