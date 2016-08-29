90cd8d9276fd66b356df309e8b2a7014750fd325
banana <178776885@qq.com>
2016-08-29 22:38:20 +0800

    client

--- client/home/controller/home_guardian.js
+++ client/home/controller/home_guardian.js
@@ -1,6 +1,28 @@
 angular.module('mobilecare').controller('HomeGuardianCtrl',
-	['$scope','$state',function ($scope,$state){
+	['$scope','$state','$mdDialog',function ($scope,$state,$mdDialog){
     
     var vm = this;
 
+//添加监护患者
+    vm.addPatientDialog = function (){
+
+    }
+
+       vm.openGuardianAddPatientDialog = function(){
+      $mdDialog.show({
+        controller: 'GuardianAddPatientCtrl',
+        controllerAs: 'gapc',
+        templateUrl: 'client/patients/views/guardian_add_patient.ng.html',
+        clickOutsideToClose:true,
+        resolve: {
+          patients: function () {
+            return $scope.patients;
+          }
+        }
+      }).then(function(answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+    };
 }]);
